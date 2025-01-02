08c9de645a1a98a42e0e6eb95a35b49c4d235259
IvanKeyiOS <ivanbeat888@gmail.com>
2025-01-02 15:28:52 +0100

    add xcode project

---
+++ CallScreenConstraints/CallScreenConstraints/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  CallScreenConstraints
+//
+//  Created by Иван Курганский on 02/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
